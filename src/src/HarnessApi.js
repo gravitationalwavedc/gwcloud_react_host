c461716acb09a21342115e2e8c868d91ae6b368b
TomReichardt <thomas.reichardt@students.mq.edu.au>
2020-08-05 14:09:15 +1000

    React Host commit for React Hooks
    Summary: Allowing modules to use react hooks through the harnessApi
    
    Test Plan: I don't know how to test this
    
    Reviewers: llakerink, mr-asher
    
    Reviewed By: llakerink, mr-asher
    
    Differential Revision: https://phab.gw-cloud.org/D71

--- src/src/HarnessApi.js
+++ src/src/HarnessApi.js
@@ -1,13 +1,25 @@
-import {getEnvironment} from "./Environment";
-import readContext from "react-relay/lib/readContext"
-import getContext from "react-relay/lib/ReactRelayContext"
-
-function setAuthTokens(token, refreshToken) {
+import React, {
+    useState,
+    useEffect,
+    useContext,
+    useReducer,
+    useCallback,
+    useMemo,
+    useRef,
+    useImperativeHandle,
+    useLayoutEffect,
+    useDebugValue
+ } from "react";
+ import {getEnvironment} from "./Environment";
+ import readContext from "react-relay/lib/readContext"
+ import getContext from "react-relay/lib/ReactRelayContext"
+  
+ function setAuthTokens(token, refreshToken) {
     localStorage.authToken = token;
     localStorage.authRefreshToken = refreshToken;
-}
-
-const HarnessApi = {
+ }
+  
+ const HarnessApi = {
     getEnvironment: getEnvironment,
     setAuthTokens: setAuthTokens,
     // retryHarnessUserDetails is set in App.jsx when Layout is rendered
@@ -16,7 +28,21 @@ const HarnessApi = {
     currentUser: null,
     // When using fragments, you must use this context and reader
     relayContext: getContext,
-    readContext: context => readContext(context)
-};
-
-export default HarnessApi;
+    readContext: context => readContext(context),
+    reactHooks: {
+        useState: useState,
+        useEffect: useEffect,
+        useContext: useContext,
+        useReducer: useReducer,
+        useCallback: useCallback,
+        useMemo: useMemo,
+        useRef: useRef,
+        useImperativeHandle: useImperativeHandle,
+        useLayoutEffect: useLayoutEffect,
+        useDebugValue: useDebugValue
+    }
+ };
+  
+ export default HarnessApi;
+ 
+ 
