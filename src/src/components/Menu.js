75819adcbc0f2399ae0b75607d974779fd84cfd5
Asher <jasherleslie@gmail.com>
2020-10-15 18:12:15 +1100

    Create app theme.
    Summary:
            - Add styles to theme.scss
            - Add a style linter
            - Update Menu
    
    Test Plan: All tests should pass and the app should look better :D
    
    Reviewers: llakerink, TomReichardt
    
    Reviewed By: llakerink
    
    Differential Revision: https://phab.adacs.org.au/D261

--- src/src/components/Menu.js
+++ src/src/components/Menu.js
@@ -1,43 +1,49 @@
-import React from "react";
+import React from 'react';
 import {logout} from '../Environment';
 import {Link} from 'found';
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import { HiOutlineUserCircle, HiOutlineLogout } from "react-icons/hi";
-import GWCloudLogo from "../assets/GWCloud-logo-primary200.png";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { HiOutlineUserCircle, HiOutlineLogout } from 'react-icons/hi';
+import GWCloudLogo from '../assets/GWCloud-logo-primary200.png';
 
 const iconStyle = {
-  height: '20px',
-  margin: '-2px 2px 0 0'
+    height: '20px',
+    margin: '-2px 2px 0 0'
 };
 
 const subMenu = (name) => {
-  if(name){
-    return (
-      <Nav>
-        <Navbar.Text className="justify-content-end mr-3"><HiOutlineUserCircle style={iconStyle}/> {name}</Navbar.Text>
-        <Nav.Link onClick={() => logout()}><HiOutlineLogout style={iconStyle}/> Logout</Nav.Link>
-      </Nav>
-    );
-  };
+    if(name){
+        return (
+            <Nav>
+                <Navbar.Text className="justify-content-end mr-3">
+                    <HiOutlineUserCircle style={iconStyle}/> {name}
+                </Navbar.Text>
+                <Nav.Link onClick={() => logout()}>
+                    <HiOutlineLogout style={iconStyle}/> Logout
+                </Nav.Link>
+            </Nav>
+        );
+    }
 
-  return (
-    <Nav>
-      <Link to="/auth/" exact>Login</Link>
-    </Nav>
-  );
-}
+    return (
+        <Nav>
+            <Link to="/auth/" exact>Login</Link>
+        </Nav>
+    );
+};
 
 const Menu = ({name}) => {
-  const SubMenu = subMenu(name);
-  return (
-    <Navbar fixed="top">
-      <Navbar.Brand className="mr-auto">
-        <Link to="/" exact style={{color: "#55BBFB"}}><img src={GWCloudLogo} style={iconStyle}/>GWCloud</Link>
-      </Navbar.Brand>
-      {SubMenu}
-    </Navbar>
-  );
-}
+    const SubMenu = subMenu(name);
+    return (
+        <Navbar fixed="top">
+            <Navbar.Brand className="mr-auto">
+                <Link to="/" exact className="navbar-brand-link">
+                    <img src={GWCloudLogo} style={iconStyle}/>GWCloud
+                </Link>
+            </Navbar.Brand>
+            {SubMenu}
+        </Navbar>
+    );
+};
 
 export default Menu;
