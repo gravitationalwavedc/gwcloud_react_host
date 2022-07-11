981260189582e6d163c883a30e9877c79fad48bf
TomReichardt <thomas.reichardt@students.mq.edu.au>
2022-07-11 16:08:02 +1000

    Fixing non-Relay route props

--- src/src/router/Resolver.js
+++ src/src/router/Resolver.js
@@ -179,7 +179,7 @@ export default class Resolver {
                     return route.render({
                         match,
                         Component: isComponentResolved ? Component : null,
-                        props: match,
+                        props: {match, router},
                     });
                 }
 
